b877d3993be85584c395323bb6b0d659f7c2e885
David Colls <dcolls@dcmbp.lan>
2023-11-16 09:00:48 +1100

    initial empty test classes

---
+++ test/test_similarity_model.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestSimilarityModel(unittest.TestCase):
+
+    def test_cosine(self):
+        pass
+
+    def test_similarity(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/test_vocabulary.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestVocabulary(unittest.TestCase):
+
+    def test_vocabulary_size(self):
+        pass
+
+    def test_remove_nsfw_words(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
