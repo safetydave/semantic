d402a862c7c1f110b7ba95125e4e7e89bd71d84a
David Colls <dcolls@dcmbp.lan>
2023-11-16 20:40:10 +1100

    refactor vocabulary to base class

--- src/vocabulary.py
+++ src/vocabulary.py
@@ -9,31 +9,21 @@ BASE_VOCABULARY_SIZE = 25487
 NSFW_WORDS = 'https://raw.githubusercontent.com/LDNOOBW/List-of-Dirty-Naughty-Obscene-and-Otherwise-Bad-Words/master/en'
 
 
-def add_missing_words(vocabulary, words):
-    added = []
-    for w in words:
-        if w not in vocabulary.words:
-            vocabulary.add(w)
-            added.append(w)
-    return added
-
-
-class VocabularyEWS:
+class Vocabulary:
 
     def __init__(self):
-        self.words = list(english_words_set)
-        self.freqs = None
+        self.words = None
 
     def add(self, word):
         self.words.append(word)
 
-
-class VocabularyUF:
-
-    def __init__(self):
-        unigram_freq_df = pd.read_csv('../data/unigram_freq.csv').set_index('word')
-        self.words = list(unigram_freq_df.index[:BASE_VOCABULARY_SIZE])
-        self.freqs = list(unigram_freq_df['count'][:BASE_VOCABULARY_SIZE])
+    def add_missing_words(self, words):
+        added = []
+        for w in words:
+            if w not in self.words:
+                self.add(w)
+                added.append(w)
+        return added
 
     def remove_nsfw_words(self):
         count = 0
@@ -44,6 +34,20 @@ class VocabularyUF:
                 count = count + 1
         return count
 
+
+class VocabularyEWS(Vocabulary):
+
+    def __init__(self):
+        Vocabulary.words = list(english_words_set)
+
+
+class VocabularyUF(Vocabulary):
+
+    def __init__(self):
+        unigram_freq_df = pd.read_csv('../data/unigram_freq.csv').set_index('word')
+        Vocabulary.words = list(unigram_freq_df.index[:BASE_VOCABULARY_SIZE])
+        self.freqs = list(unigram_freq_df['count'][:BASE_VOCABULARY_SIZE])
+
     def add(self, word):
-        self.words.append(word)
+        Vocabulary.add(word)
         self.freqs.append(0)
