231458e35dfa0730134193482746feb5062b8a6a
David Colls <dcolls@dcmbp.lan>
2023-11-16 13:05:49 +1100

    implement some vocabulary tests

--- test/test_vocabulary.py
+++ test/test_vocabulary.py
@@ -1,13 +1,23 @@
 import unittest
 
+from src.vocabulary import VocabularyEWS, VocabularyUF
+
 
 class TestVocabulary(unittest.TestCase):
 
-    def test_vocabulary_size(self):
-        pass
+    def test_vocabulary_size_should_be_human_typical(self):
+        v_ews = VocabularyEWS()
+        self.assertGreaterEqual(len(v_ews.words), 25000)
+        self.assertLessEqual(len(v_ews.words), 30000)
+        v_uf = VocabularyUF()
+        self.assertGreaterEqual(len(v_uf.words), 25000)
+        self.assertLessEqual(len(v_uf.words), 30000)
 
-    def test_remove_nsfw_words(self):
-        pass
+    def test_nsfw_words_should_be_removed(self):
+        v_uf = VocabularyUF()
+        self.assertIn('ass', v_uf.words)
+        v_uf.remove_nsfw_words()
+        self.assertNotIn('ass', v_uf.words)
 
 
 if __name__ == '__main__':
